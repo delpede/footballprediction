ae97df3ef27f2ba1c55da8e8506aac1a02d5f32a
Peter Larsen <p.larsen@elsevier.com>
2019-06-16 19:28:11 +0200

    Working towards login

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 class Users(UserMixin, db.Model):
     # todo model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(128))
     email = db.Column(db.String(50))
     phone = db.Column(db.String(20))
     roles = db.Column(db.Integer)
+    verified = db.Column(db.Integer)
+
+    # https://dev.to/kaelscion/authentication-hashing-in-sqlalchemy-1bem
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'YouShouldProbablyChangeThis'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'YouShouldProbablyChangeThis'
